877f6d24d1c848ba7048bc10c1347fef77e785cf
kol845 <pebo-hamza@hotmail.com>
2021-01-25 13:01:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Run command:
+php -S localhost:8080 -t public public/index.php
---
+++ src/Api.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+use PDO;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class Api
+{
+    private App $app; # App from 'skin' framework
+
+    /**
+     * Api constructor.
+     */
+    public function __construct()
+    {
+        $this->app = AppFactory::create(); # Slim framework stuff
+
+        $this->setup($this->app); # Run setup()
+    }
+
+    public function run()
+    {
+        $this->app->run();
+    }
+
+    function getDatabaseSettings(): DatabaseSettings
+    {
+        $settings = new DatabaseSettings();
+        $settings->username = getenv('MYSQL_USERNAME');
+        $settings->password = getenv('MYSQL_PASSWORD');
+        $settings->host = getenv('MYSQL_HOST');
+        $settings->database = getenv('MYSQL_DATABASE');
+        $settings->sslCa = getenv('MYSQL_SSL_CA');
+        $settings->sslKey = getenv('MYSQL_SSL_KEY');
+        $settings->sslCert = getenv('MYSQL_SSL_CERT');
+
+        if (!file_exists($settings->sslCa) || !file_exists($settings->sslKey) || !file_exists($settings->sslCert)) {
+            error_log("Missing MySQL SSL files");
+            die();
+        }
+
+        return $settings;
+    }
+
+    private function setup(App $app): App
+    {
+        $pdo = (new PdoFactory($this->getDatabaseSettings()))->createPdo(); # PHP Database Object. From PHP DB.
+        $counterApi = new CounterApi(new CounterService($pdo)); # Create a api instance with pdo as settings
+
+        $app->options('/{routes:.*}', function (Request $request, Response $response) {
+            // CORS Pre-Flight OPTIONS Request Handler
+            return $response;
+        });
+
+        $app->group('/api/counters', function (Group $group) use ($counterApi) {
+            $counterApi->setup($group);
+        });
+
+        return $app;
+    }
+}
