04b41b1d0f3415b1ea4bb6977aeba943acfc5c6e
kol845 <pebo-hamza@hotmail.com>
2021-02-11 15:27:49 +0100

    Heroku fix 1

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 
 use App\Api; #Import the Api class from App(src folder)
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 (new Api())->run(); # create new instance of Api and run run()
