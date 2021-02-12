4a0ba5a308a07993c62a287db962fe3fdbfd5704
kol845 <pebo-hamza@hotmail.com>
2021-02-12 09:34:26 +0100

    Heroku fix 6

--- src/Api.php
+++ src/Api.php
@@ -49,7 +49,7 @@ class Api
 
         $app->options('/{routes:.*}', function (Request $request, Response $response) {
             // CORS Pre-Flight OPTIONS Request Handler
-            return $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
+            return $response->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         });
